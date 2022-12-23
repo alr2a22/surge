b5e47dd6bc9e07c3fbd81d6dc36caca0621a6596
alrzazz <alirezaazizi1396622@gmail.com>
2022-12-23 17:56:01 +0330

    FIx scripts directory name

--- scripts/import.sh
+++ scripts/import.sh
@@ -3,7 +3,7 @@ ogr2ogr_image=registry.docker.ir/osgeo/gdal:alpine-small-latest
 data_path=$home/data/districts.geojson
 config_file=$home/config/.env
 export $(grep -v '^#' $config_file | xargs)
-db_connection="host=$POSTGRES_HOST dbname=$POSTGRES_DB user=$POSTGRES_USER password=$POSTGRES_PASSWORD"
+db_connection="host=127.0.0.1 dbname=$POSTGRES_DB user=$POSTGRES_USER password=$POSTGRES_PASSWORD"
 
 docker run --rm -v=$data_path:/data.geojson --network=host $ogr2ogr_image ogr2ogr -f "PostgreSQL" PG:"$db_connection" /data.geojson -nln districts
 
